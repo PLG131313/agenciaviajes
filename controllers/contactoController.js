3a45ab73c0dc5bb2b749839ad3075d50fc21e713
Pablo1313 <pablo12345678902005@gmail.com>
2025-02-19 12:05:36 +0100

    Correccion

--- controllers/contactoController.js
+++ controllers/contactoController.js
@@ -1,23 +1,22 @@
 import { Contacto } from "../models/contacto.js";
 
 export const guardarMensaje = async (req, res) => {
-    const { nombre, correo, mensaje } = req.body;
+    const { nombre, correo, mensaje, valoracion } = req.body;
 
-    if (!nombre || !correo || !mensaje) {
+    if (!nombre || !correo || !mensaje || !valoracion) {
         return res.render("contacto", {
             errores: [{ mensaje: "Todos los campos son obligatorios" }],
             nombre,
             correo,
-            mensaje
+            mensaje,
+            valoracion
         });
     }
 
     try {
-        await Contacto.create({ nombre, correo, mensaje });
-        res.render("contacto", { mensajeExito: "Mensaje enviado con éxito" });
+        await Contacto.create({ nombre, correo, mensaje, valoracion });
+        res.redirect("/contacto");
     } catch (error) {
         console.error(error);
-        res.render("contacto", { errores: [{ mensaje: "Hubo un error al enviar el mensaje" }] });
     }
 };
-
