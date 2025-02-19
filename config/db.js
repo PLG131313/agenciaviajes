4785b084376412a44cba0bc1fee32f888d3f212a
Pablo1313 <pablo12345678902005@gmail.com>
2025-02-19 09:10:29 +0100

    Errores

--- config/db.js
+++ config/db.js
@@ -1,18 +1,16 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
+import Sequelize from 'sequelize';
+import  dotenv from 'dotenv';
 dotenv.config();
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    define: { timestamps: true },
-    pool: {
-        max: 3,
-        min: 0,
-        acquire: 20000,
-        idle: 5000
+const db = new Sequelize(process.env.CONEXION,{
+    define: {
+        timestamps: true,
     },
-    logging: false
+    pool:{
+        max: 5,
+        min: 0,
+        acquire: 3000,
+        idle: 1000
+    },
 });
-
 export default db;
